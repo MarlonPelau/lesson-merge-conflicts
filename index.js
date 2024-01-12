0bab315366cab48f96b608143a8ab866c266b997
MarlonPelau <MarlonRegis@pursuit.org>
2024-01-12 11:30:51 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function createPerson(name, age) {
+    return {
+      name,
+      age,
+      friends: [],
+    };
+  }
+  
+  function addPersonAsFriend(person, other) {
+    person.friends.push(other);
+    return person;
+  }
+  
