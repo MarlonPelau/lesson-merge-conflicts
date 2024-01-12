86488e88a62c81e428ef87f0f5dde243d94b86ca
MarlonPelau <MarlonRegis@pursuit.org>
2024-01-12 11:45:30 -0500

    trying to show changed branch results

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function createPerson(name, age) {
     };
   }
   
-  function addPersonAsFriend(person, other) {
-    person.friends.push(other);
-    return person;
+  function addPersonAsFriend(personA, personB) {
+    personA.friends.push(personB);
+    return personA;
   }
   
